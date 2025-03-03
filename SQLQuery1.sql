8373afb5964b13f672c59ea2ba3ccde47df8fc5c
KŽ <kristopherzuravljov005@gmail.com>
2025-03-03 11:42:10 +0200

    Add files via upload

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -17,4 +17,93 @@ values (1, 'female')
 insert into Gender (Id, Gender)
 values (3, 'Unknown')
 
+--vaatame tabeli sisu
+select * from Gender
 
+--teeme tabeli Person
+
+create table Person
+(
+Id int not null primary key,
+Name nvarchar(30),
+Email nvarchar(30),
+GenderId int
+)
+--andmete sisestamine
+insert into Person (Id, Name, Email, GenderID)
+values (1, 'Napoleon', 'Na.com', 2)
+insert into Person (Id, Name, Email, GenderID)
+values (2, 'Alexander', 'Al.com', 2),
+(3, 'Caesar', 'CS.com', 2),
+(4, 'Lionheart', 'LH.com', 2),
+(5, 'Friedrick', 'FR.com', 2),
+(6, 'Abraham', 'AB.com', 2),
+(7, 'Lennart', 'LN.com', 2),
+(8, 'Laidoner', 'LR.com', 2),
+(9, NULL, NULL, 2)
+
+--soovime vaadata Person tabeli andmeid
+select * from Person 
+
+-- võõrvõtme ühenduse loomine kahe tabeli vahel
+alter table Person add constraint tblPerson_GenderId_FK
+foreign key (GenderId) references Gender(Id)
+
+--kui sisestad uue rea andmeid ja ei ole sisestanud GenderId alla väärtust
+--siis see automaatslet sisestab sellele reale väärtuse 3 e nagu meil
+-- on unknown
+alter table Person
+add constraint DF_Persons_GenderId
+default 3 for GenderId
+--
+insert into Person (Id, Name, Email)
+values (10, 'Cleopatra', 'CL.com')
+
+insert into Person (Id, Name, Email)
+values (11, 'delete', 'del.com')
+
+-- piirangu kustutamine
+alter table person
+drop constraint DF_Persons_GenderId
+
+-- lisame uue veeru
+alter table Person
+add age nvarchar(10)
+
+--lisame nr piirangu vanuse sisestamisel
+alter table Person
+add constraint CK_Person_Age check (Age > 0  and Age < 155)
+
+--kustutame rea
+delete from Person where Id = 11
+
+select * from Person
+
+--kuidas uuendada andmeid 
+update Person
+set Age = 30 
+where Id = 4
+
+alter table Person
+add City nvarchar(50)
+
+--kõik kes elavad Tallinnas
+select*from Person where City = 'Tallinn'
+--kõik kes ei ela Tallinnas
+select*from Person where City != 'Tallinn'
+
+--aitab teatud vanusega inimesi
+select * from Person where Age = 70 or Age = 30 or Age = 20
+
+
+--aitab teatud vanusevahemikus olevaid inimesi
+select * from Person where (Age > 0  and Age < 50)
+select * from Person where Age between 0 and 30
+
+-- wildcard e näitab kõik g-tähega linnad
+select * from Person where City like 't%'
+--kõik emailid, kus on @-mrk emailis
+select * from Person where Email like '%@%'
+
+--näitab kõiki, kellel ei ole @-märki emailis
+select * from Person where Email not like '%@%'
