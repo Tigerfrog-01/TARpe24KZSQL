a631476aa891604f78c17bef4f1bc5a9adfd74e6
KŽ <kristopherzuravljov005@gmail.com>
2025-03-03 09:37:56 +0200

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,20 @@
+--loome db
+create database TARpe24SQL
+-- db valimine
+use TARpe24SQL
+--tabeli loomine
+create table Gender
+(
+Id int not null primary key,
+Gender nvarchar(10) not null
+)
+
+--andmete sisestamine
+insert into Gender (Id, Gender)
+values (2, 'male')
+insert into Gender (Id, Gender)
+values (1, 'female')
+insert into Gender (Id, Gender)
+values (3, 'Unknown')
+
+
