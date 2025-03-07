d48bdc87f0f233b2a4691ea2dd6fe2ade831a703
KŽ <kristopherzuravljov005@gmail.com>
2025-03-07 15:50:55 +0200

    Add files via upload

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -75,7 +75,7 @@ alter table Person
 add constraint CK_Person_Age check (Age > 0  and Age < 155)
 
 --kustutame rea
-delete from Person where Id = 11
+delete from Person where Id = 9
 
 select * from Person
 
@@ -107,3 +107,108 @@ select * from Person where Email like '%@%'
 
 --näitab kõiki, kellel ei ole @-märki emailis
 select * from Person where Email not like '%@%'
+
+-- tund 2 07.03.2025
+-- näitab kellel on emailis ees ja peale @-märki ainult üks täht
+select * from Person where Email like '_@_.com'
+--kõik kellel on nimes esimene täht W, A, S
+select * from Person where name like '[^N]%'
+select * from Person
+
+--kes elavad Roomas ja Tallinnas
+select * from Person where City = 'Rome' or City = 'Tallinn'
+
+--kõik, kes elavad Roomas ja Tallinnas ja on vanemad kui 29
+select * from Person where City = 'Rome' or City = 'Tallinn' and (Age > 29) 
+
+--kuvab tähestikulises järjekorras inimesi ja võtab auseks nime
+
+select * from Person order by name
+
+--sama päring, aga vastupidi järjestuses on nimed
+
+select * from Person order by name desc
+
+--võtab kolm esimest rida
+
+select top 3 * from Person
+
+--kolm esimest, aga tabeli järjestus on Age ja siis Name
+
+select top 3 Age, name From Person
+
+--näitab esimesed 50% tabelis
+
+select top 50 percent * from Person 
+
+--järjestame vanuse järgi isikud
+
+select * from Person order by Age desc
+
+--muudab Age muutuja intiks
+
+select * from Person order by cast(Age as int)
+
+--kõikide isikute koondvanus
+
+select sum(cast(Age as int)) from Person
+
+--kuvab kõige nooremat isikud
+
+SELECT TOP 1 * FROM Person ORDER BY Age;
+
+select min(cast(Age as int)) from Person
+
+--kuvab kõige vanemat isikut
+
+select max(cast(Age as int)) from Person
+
+SELECT TOP 1 * FROM Person ORDER BY Age desc;
+
+--konkreetsetes linnades olevate isikute koondvanus
+-- enne oli Age nvarchar, aga muudame selle int andmetüübiks
+
+select City, sum(Age) as totalAge from Person group by City
+
+--kuidas saab koodiga muuta andmetüüpi ja sellest pikkust
+
+alter table Person
+alter column Name nvarchar(30)
+
+--kuvab esimes reas välja toodud järjestuses ja kuvab TotalAge-ks
+-- järjestab City-s olevate nime järgi ja siis GenderId järgi
+
+select City, GenderId, sum(Age) as TotalAge from Person
+group by City, GenderId order by City
+
+--näitab ridade arvu tabelis
+
+select count(*) from Person 
+Select * from Person
+
+--näitab tulemust, et mitu inimest on genderId väärtusega 2 konkreetses linnas
+-- arvutab vanuse kokku selles linnas
+
+select GenderId, city, sum(Age) as TotalAge, count(Id) as [Total Person(s)]
+from Person 
+Where GenderId = '2'
+group by GenderId, City
+
+--loome, tabelid Employees ja Department
+
+create table Department
+(
+Id int primary key,
+DepartmentName nvarchar(50),
+Location nvarchar(50),
+DepartmentHead nvarchar(50)
+)
+
+create table Employees
+(
+Id int primary key,
+Name nvarchar(50),
+Gender nvarchar(50),
+Salary nvarchar(50),
+DepartmentId int
+)
