ec5e0e22298aaa09eb05722e8ee181d14ed3fcce
KŽ <kristopherzuravljov005@gmail.com>
2025-03-17 11:45:02 +0200

    Add files via upload

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -522,3 +522,193 @@ as begin
 
  sp_helptext spGetEmployeesByGenderAndDepartment 
   
+ --kuidas muuta sp-d ja pane krüpteeringu peale, et keegi teine peale teid ei saaks muuta
+ alter proc spGetEmployeesByGenderAndDepartment
+ @Gender nvarchar(20),
+ @DepartmentID int
+ with encryption
+ as begin
+ select FirstName, Gender, DepartmentID from Employees where Gender = @Gender
+ and DepartmentId = @DepartmentID
+ end
+
+ sp_helptext spGetEmployeesByGenderAndDepartment
+
+ --sp tegemine
+ create proc spGetEmployeeCountByGender
+ @Gender nvarchar(20),
+ @EmployeeCount int output
+ as begin
+ select @EmployeeCount = count(Id) from Employees where Gender = @Gender
+ end
+ --annab tulemuse, kus loendab ära nõuetele vastavad read
+ --prindib tulemuse konsooli
+ declare @TotalCount int
+ execute spGetEmployeeCountByGender 'Female',@TotalCount out
+ if(@TotalCount = 0)
+ print 'TotalCount is null'
+ else
+ print '@Total is not null'
+ print @TotalCount
+
+--näitab ära, et mitu rida vastab nõutele
+declare @TotalCount int 
+exec spGetEmployeeCountByGender @EmployeeCount = @TotalCount out, @Gender = 'Male'
+print @TotalCount
+
+--sp sisu vaatamine??
+
+sp_help spGetEmployeeCountByGender
+
+--tabeli info
+sp_help Employees
+
+
+--kui soovid sp teksti näha
+sp_helptext spGetEmployeeCountByGender
+
+--vaatame, millest see sp sõltub
+sp_depends spGetEmployeeCountByGender
+--vaatame tabelit
+sp_depends Employees
+
+
+
+--
+create proc spGetnameById
+@Id int,
+@Name nvarchar(20) output
+as begin
+    select @Id = Id, @Name = FirstName from Employees
+end
+
+select * from Employees
+
+ declare @FirstName nvarchar(50)
+ execute spGetnameById 1, @FirstName out
+print 'Name of the emploee = ' + @FirstName
+
+--mis id all on keegi nime järgi
+
+create proc SpGetNameById1
+@Id int,
+@firstName nvarchar(50) output
+as begin
+select @FirstName = FirstName from Employees where Id = @Id
+end
+
+ declare @FirstName nvarchar(50)
+ execute spGetnameById1 4, @FirstName out
+print 'Name of the emploee = ' + @FirstName
+
+sp_help spGetNameById1
+
+---
+create proc spGetNameById2
+@Id int 
+as begin
+return (select FirstName from Employees where Id = @Id)
+end
+
+ declare @FirstName nvarchar(50)
+ execute @FirstName = spGetNameById2 1
+print 'Name of the emploee = ' + @FirstName
+
+
+--ei saa convertida nvarchar mille väärtus on Tom , int tüübi väärtusele
+
+--sisse ehitatud string funktsioonid
+-- see konverteerib ASCII tähe väärtuse numbriks
+select ascii('a')
+--kuvab A-tähe
+select char (65)
+--prindime kogu tähestiku välja
+declare @Start int
+set @Start = 97
+while (@Start <=122)
+begin
+select char (@Start)
+set @Start = @Start + 1
+end
+
+--eemaldame tühjad kohad sulgudes
+select ltrim('                     Hello')
+
+--tühikute eemaldamine veerust
+select ltrim(FirstName) as FirstName, MiddleName, LastName from Employees
+
+select * from Employees 
+
+--paremalt poolt tühjad stringid lõikab ära
+select rtrim('              Hello        ')
+--keerab kooloni sees olevad andmed vastupidiseks
+--vastavalt upper ja lower-ga saan muuta märkide suurust
+--reverse funktsioon pöörab kõi ümber
+
+select REVERSE(UPPER(ltrim(FirstName))) as FirstName, MiddleName, lower(LastName), 
+rtrim(ltrim(FirstName)) + '' +	MiddleName + '' + LastName as FullName
+from Employees
+
+--näeb, mitu tähte on sõnal ja loeb tühikud sisse
+select FirstName, len(FirstName) as [Total Characters] from Employees
+
+--näeb, mitu tähte on sõnal ja ei loe tühikuid sisse
+select (FirstName), len(ltrim(FirstName)) as [Total Characters] from Employees
+
+--left, right ja substring 
+-- vasakult poolt neli esimest tähte
+select left('ABCDEF',4)
+--paremalt poolt kolm tähte
+select right('ABCDEF',3 )
+
+--kuvab @-tähemärgi asetust
+select charindex('@', 'sara@aaa.com')
+
+--esimene nr peale komakohta näitab, et mitmendat alustab ja siis mitu nr peale 
+-- seda kuvada 
+select SUBSTRING('pam@bb.com', 5, 2)
+
+--@-märgist kuvab kolm tähemärki. viimase numbirga saab määrata pikkust
+select substring('pam@bbb.com', charindex('@', 'pam@bbb.com') + 1, 3)
+
+--peale @-märki reguleerin tähemärkide pikkuse näitamist
+select substring('pam@bbb.com', charindex('@', 'pam@bbb.com') + 1,
+len('pam@bbb.com') - CHARINDEX('@', 'pam@bb.com'))
+
+
+select * from Employees
+
+--vaja teha uus veerg nimega Email, nvarchar (20)
+
+alter table Employees
+add Email nvarchar(20)
+
+update Employees set Email =  'Tom@aaa.com' where Id = 1
+update Employees set Email =  'Pam@aaa.com' where Id = 2
+update Employees set Email =  'John@ccc.com' where Id = 3
+update Employees set Email =  'Sam@bbb.com' where Id = 4
+update Employees set Email =  'Todd@aaa.com' where Id = 5
+update Employees set Email =  'Ben@ccc.com' where Id = 6
+update Employees set Email =  'Sara@bbb.com' where Id = 7
+update Employees set Email =  'Valarie@ccc.com' where Id = 8
+update Employees set Email =  'James@bbb.com' where Id = 9
+update Employees set Email =  'Russel@bbb.com' where Id = 10
+
+---lisame tärni *-märgi alates teatud kohast
+select FirstName, LastName, 
+substring(Email, 1, 2) + REPLICATE('*', 5) + 
+SUBSTRING(Email, CHARINDEX('@', Email), len(Email) - charindex('@', Email)+1) as Email
+from Employees
+
+---kolm korda näitab stringis olevat väärtust
+
+SELECT REPLICATE(Email, 3)
+FROM Employees;
+
+--kuidas sisestada tühikut kahe nime vahele
+select space(5)
+
+--Employee tabelist teed päringu kahe nime osas (FirstName, LastName)
+--kahe nime vahel on 25 tühikut
+select FirstName + space(25) + LastName as FullName
+from Employees
